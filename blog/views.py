1231b1855bb629c825349b50fdfdf9a5b9e7616b
Penny Train <penny.train04@gmail.com>
2024-01-11 13:23:53 +0000

    added post-detail view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
@@ -23,7 +22,6 @@ def post_detail(request, slug):
 
     :template:`blog/post_detail.html`
     """
-
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     return render(request, "blog/post_detail.html", {"post": post},)
