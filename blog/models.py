d7620b5fb64de5fa384e0399d40beb56b3018b11
Penny Train <penny.train04@gmail.com>
2024-01-16 15:07:38 +0000

    enable serving of image files

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,8 @@ class Post(models.Model):
     User, on_delete=models.CASCADE, 
 related_name="blog_posts"
 )
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', 
+default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, 
